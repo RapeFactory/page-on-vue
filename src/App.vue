5709b88ff5741ee0d885233fdcc7e375a18f93c6
Alexandr Makeev <ssh11ssh@gmail.com>
2018-08-16 16:00:54 +0300

    navbar style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,20 +9,34 @@
 </template>
 
 <style lang="scss">
+$maingrey: #2c3e50;
+$maingreen: #42b983;
 #app {
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $maingrey;
 }
 #nav {
-  padding: 30px;
+  padding: 5px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.7);
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $maingrey;
+    line-height: 3;
+    border-radius: 5px;
+    background-color: lighten($maingrey, 50%);
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.7);
+    margin: 3px;
+    width: 150px;
+    height: 50px;
+    display: inline-block;
+    transition: all 0.5s ease;
     &.router-link-exact-active {
-      color: #42b983;
+      color: $maingreen;
+      background-color: lighten($maingreen, 30%);
     }
   }
 }
