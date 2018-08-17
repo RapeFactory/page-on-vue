52bf1d98ab8eed6f5c7330be87b46f735d79cd4d
Alexandr Makeev <ssh11ssh@gmail.com>
2018-08-17 14:01:28 +0300

    Post Component

---
+++ src/components/Post.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post">
+    <div class="title"><h3>{{title}}</h3></div>
+    <div class="body">{{body}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    margin: 10px;
+    padding: 10px;
+    text-align: left;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.7);
+  }
+</style>
