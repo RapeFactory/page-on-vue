fdf50e17bcc42f67b02e100e4cd48c0c2002305f
Alexandr Makeev <ssh11ssh@gmail.com>
2018-08-17 14:00:09 +0300

    store fetch posts

--- src/store.js
+++ src/store.js
@@ -1,10 +1,32 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    posts: [],
+    lastPostId: 0,
+  },
+  mutations: {
+    updatePosts(state, payload) {
+      console.log('updatePosts');
+      const { lastPostId, posts } = payload;
+      state.posts = [...state.posts, ...posts];
+      state.lastPostId = lastPostId;
+    },
+  },
+  actions: {
+    fetchPosts({ commit }) {
+      const url = 'https://jsonplaceholder.typicode.com/posts';
+      console.log('fetchPosts');
+      fetch(url)
+        .then(res => res.json())
+        .then(data => (commit('updatePosts', {
+          lastPostId: data.pop().id,
+          posts: data,
+        })))
+        .catch(console.error);
+    },
+  },
 });
